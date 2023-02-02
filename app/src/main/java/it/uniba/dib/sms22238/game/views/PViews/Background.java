3187d085634f00487aae4f69c190a316b2f2e448
Benedettatotaro <benedettatotaro@yahoo.it>
2023-02-02 13:23:24 +0100

    ciao

---
+++ app/src/main/java/it/uniba/dib/sms22238/game/views/PViews/Background.java
@@ -0,0 +1,20 @@
+package it.uniba.dib.sms22238.game.views.PViews;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import it.uniba.dib.sms22238.R;
+
+public class Background {
+
+    public Bitmap background;
+    public int x=0,y=0;
+
+    public Background(Resources res, int screenX, int screenY){
+
+        background= BitmapFactory.decodeResource(res, R.drawable.travel_background);
+        //background=Bitmap.createScaledBitmap(background,screenX,screenY,false);
+
+    }
+}
